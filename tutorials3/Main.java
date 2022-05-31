498f3ed658b3247e3bea4851e3ca09b2c70d17e0
coolmarvel <marvel97@naver.com>
2022-05-31 15:08:46 +0900

    자료형, 형변환

---
+++ tutorials3/Main.java
@@ -0,0 +1,40 @@
+package tutorials3;
+
+/**
+ * Main
+ */
+public class Main {
+    // 자료형
+    // 형변환 (int) (double) etc...
+
+    public static void main(String[] args) {
+        // double b = 0.5;
+        // int a = (int) (b + 0.5);
+        // System.out.println(a);
+
+        // 평균값 구하기
+        // double a = 10.3;
+        // double b = 9.6;
+        // double c = 10.1;
+        // System.out.println((a + b + c) / 3);
+
+        // ASCI기반 char로 A~Z까지 출력
+        // for (char i = 'a'; i <= 'z'; i++) {
+        // System.out.println(i);
+        // }
+
+        // 10진수를 8진수 혹은 16진수로 바꾸어 출력
+        // int a = 200;
+        // System.out.println("10진수 : " + a);
+        // System.out.format("8진수 : %o" + a);
+        // System.out.format("16진수 : %x" + a);
+
+        // String의 substring을 활용
+        // String name = "John Doe";
+        // System.out.println(name);
+        // System.out.println(name.substring(0, 1));
+        // System.out.println(name.substring(3, 6));
+        // System.out.println(name.substring(0, 4));
+        // System.out.println(name.substring(5, 8));
+    }
+}
