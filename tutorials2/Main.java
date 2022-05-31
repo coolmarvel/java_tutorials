d424a920a1cb720b568a59e54e0c15b4bb69b4da
coolmarvel <marvel97@naver.com>
2022-05-31 14:27:37 +0900

    first commit

---
+++ tutorials2/Main.java
@@ -0,0 +1,31 @@
+/**
+ * Main
+ */
+public class Main {
+    // final 상수를 정의
+    final static double PI = 3.141592;
+    final static int INT_MAX = 2147483647;
+    final static int INT_MIN = -2147483647;
+
+    public static void main(String[] args) {
+        // int intType = 100;
+        // double doubleType = 150.5;
+        // String stringType = "이성현";
+        // System.out.println(intType);
+        // System.out.println(doubleType);
+        // System.out.println(stringType);
+
+        // int r = 30;
+        // System.out.println(r * r * PI);
+
+        // int a = INT_MAX;
+        // int b = INT_MIN;
+        // System.out.println(a); // a + 1은 오버플로우로 -1이 출력
+        // System.out.println(b);
+
+        // int a = 1;
+        // int b = 2;
+        // System.out.println("a + b = " + (a + b)); // a + b = 12
+        // System.out.println("a + b = " + a + b); // a + b = 3
+    }
+}
