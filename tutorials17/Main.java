ca8bd15b021cf6d79606234d5d48fa71f2ce7d3a
coolmarvel <marvel97@naver.com>
2022-06-02 11:35:10 +0900

    클래스 Object

---
+++ tutorials17/Main.java
@@ -0,0 +1,11 @@
+package tutorials17;
+
+public class Main {
+    public static void main(String[] args) {
+        Archer archer1 = new Archer("궁수1", "상");
+        Archer archer2 = new Archer("궁수1", "상");
+
+        System.out.println(archer1 == archer2);
+        System.out.println(archer1.equals(archer2));
+    }
+}
